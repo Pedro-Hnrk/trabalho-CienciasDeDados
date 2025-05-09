bffd1120d6321d2e8653debd615f38becf1fa04b
Pedro_Hnrk <lpeterz94@outlook.com>
2025-05-09 17:55:42 -0300

    .

--- .py/limpeza-redução-de-dados.py
+++ .py/limpeza-redução-de-dados.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Carregar o arquivo CSV
-df = pd.read_csv('startup_data.csv')  # Substitua pelo caminho do seu arquivo
+df = pd.read_csv('startup_data.csv')
 
 # Verificar valores nulos em cada coluna
 print(df.isnull().sum())
